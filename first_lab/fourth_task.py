f4bc16d08b626a720bf57b17058398fa7b29b6f0
Danil Protsenko <dan.protsenko@gmail.com>
2021-02-15 21:59:59 +0600

    lab 1 finished task 4

--- first_lab/fourth_task.py
+++ first_lab/fourth_task.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from nycflights13 import flights
+
 
 def split_and_save_big_df(big_df):
     splitted = np.array_split(big_df, 4)
@@ -19,7 +21,7 @@ def read_splitted_by_dir(dir_path):
     return df
 
 
-if __name__ == '__main__':
+def first_task():
     big_df = pd.DataFrame({
         'Test': np.ones(1000000)
     })
@@ -27,3 +29,35 @@ if __name__ == '__main__':
     dir_path = './target'
     read = read_splitted_by_dir(dir_path)
     print(read)
+
+
+def count_by_carrier():
+    counted = flights.groupby(['carrier'], as_index=False).agg(count=('flight', 'count'))
+    print(counted)
+
+
+def first_and_last_five():
+    df = flights[['dep_time', 'dep_delay', 'arr_time', 'carrier', 'tailnum']]
+    print(df.head(5).append(df.tail(5)))
+
+
+def means_by_carrier():
+    aggregated = flights.groupby(['carrier'], as_index=False) \
+        .agg(mean_arr_delay=('arr_delay', 'mean'),
+             mean_dep_delay=('dep_delay', 'mean'))
+    print(aggregated)
+
+
+def transpose():
+    df = np.ones((3, 100), int)
+    print(df)
+    df_transposed = df.transpose()
+    print(df_transposed)
+
+
+if __name__ == '__main__':
+    first_task()
+    count_by_carrier()
+    first_and_last_five()
+    means_by_carrier()
+    transpose()
